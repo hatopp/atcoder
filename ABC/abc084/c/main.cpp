eaafa108c7b12c209cfdca72fee29daaf481ad75
hatopopo <hatopopo142@gmail.com>
2021-12-05 12:36:06 +0900

    remove_cdefg

--- ABC/abc084/c/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    int a, b, c, d, x, y, z, n, count, ans;
-    string s, sans;
-    cin >>
--- ABC/abc218/e/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-    int a, b, c, d, x, y, z, n;
-    cin >>
